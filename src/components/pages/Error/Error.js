91131049f020f74bc67c74bf8a0b22124b754db1
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-09-29 21:55:48 -0500

    Error Page styling

--- src/components/pages/Error/Error.js
+++ src/components/pages/Error/Error.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import "./Error.scss";
 
 const Error = () => {
   return (
     <div className="error_container">
       <h1>Invalid Link</h1>
-      <Link to="/">Click to return</Link>
+      <Link to="/"><BsFillArrowLeftCircleFill /></Link>
     </div>
   );
 };
--- src/components/pages/History/History.js
+++ src/components/pages/History/History.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import "./History.scss";
 
 const History = () => {
-  return <div className="history_container">COMING SOON...</div>;
+  return (
+    <div className="history_container">
+      COMING SOON...
+      <Link to="/"><BsFillArrowLeftCircleFill /></Link>
+    </div>
+  );
 };
 
 export default History;
